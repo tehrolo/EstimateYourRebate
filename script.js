854f722cb8dc6ff496c7fe87805fb2753b3296c0
tehrolo <teheoleo@gmail.com>
2023-10-30 10:24:33 -0500

    Update script.js

--- script.js
+++ script.js
@@ -6,24 +6,17 @@ const secondaryFeeValue = document.getElementById("secondaryFeeValue");
 const commissionValue = document.getElementById("commissionValue");
 const purchasePrice = document.getElementById("buySellPrice");
 const total = document.getElementById("total");
-var grossCommissionIncome;
-var brokerageSplit;
-var primaryFee;
-var secondaryFee
-var netIncome;
-var estimatedRebate;
-var adjustedCommission;
-var total;
 
 function updateResults() {
-    calculate()
-    primaryFeeValue.textContent = primaryFee;
-    secondaryFeeValue.textContent = secondaryFee;
-    yourRebateValue.textContent = estimatedRebate;
-    commissionValue.textContent = commissionValue;
-    total.textContent = total;
-}
-function calculate() {
+    var grossCommissionIncome;
+    var brokerageSplit;
+    var primaryFee;
+    var secondaryFee
+    var netIncome;
+    var estimatedRebate;
+    var adjustedCommission;
+    var total;
+    
     grossCommissionIncome = (purchasePrice.value * (sliderValue.value / 100));
     brokerageSplit = (grossCommissionIncome * 0.93);
     netIncome = (brokerageSplit * 0.95);
@@ -32,11 +25,18 @@ function calculate() {
     estimatedRebate = (netIncome * 0.4);
     commissionValue = (netIncome - estimatedRebate);
     total = (primaryFee + secondaryFee + estimatedRebate + commissionValue)
+    
+    primaryFeeValue.textContent = primaryFee;
+    secondaryFeeValue.textContent = secondaryFee;
+    yourRebateValue.textContent = estimatedRebate;
+    commissionValue.textContent = commissionValue;
+    total.textContent = total;
 }
+
 slider.addEventListener("input", function () {
     sliderValue.textContent = slider.value;
-    updateResults()
+    updateResults();
 });
 purchasePrice.addEventListener("input", function () {
-    updateResults()
+    updateResults();
 });
