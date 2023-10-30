9d6d745ee1f38b62913bca55c24ff6c665d14bdf
tehrolo <teheoleo@gmail.com>
2023-10-30 08:11:10 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 const slider = document.getElementById("slider");
 const sliderValue = document.getElementById("sliderValue");
+const yourRebateValue = document.getElementById("yourRebateValue");
+const primaryFeeValue = document.getElementById("primaryFeeValue");
+const secondaryFeeValue = document.getElementById("secondaryFeeValue");
+const commissionValue = document.getElementById("commissionValue");
 
 slider.addEventListener("input", function () {
     sliderValue.textContent = slider.value;
