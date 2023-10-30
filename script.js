a169b28c3c59abb6e4265cb6a734860cebb20618
tehrolo <teheoleo@gmail.com>
2023-10-29 22:29:29 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,30 +1,6 @@
-$(document).ready(function(){
-    alert("hi");}
-//vars from user input
-var buyerOrSeller;  //radio buttons
-var purchasePrice;  //text box
+const slider = document.getElementById("slider");
+const sliderValue = document.getElementById("sliderValue");
 
-var userDonate;  //checkbox
-
-
-
-//vars for Rebate calculation
-var GrossCommissionIncome = purchasePrice * commissionRate
-var BrokerageSplit = GrossCommissionIncome * 0.93
-var NetIncome = BrokerageSplit * 0.95
-
-var EstimatedRebate = NetIncome * 0.4;
-var EstimatedDonation;
-
-if(userDonate == True) {
-    var EstimatedRebate = NetIncome * 0.25;
-    var EstimatedDonation=NetIncome * 15;
-}
-
-//update slider
-var commissionRateSlider = document.getElementById("commisionRangeSlider");  //slider
-var userDefCommRate = document.getElementById("adjCommRate");
-userDefCommRate.innerHTML = commissionRateSlider.value; // Display the default slider value
-commissionRateSlider.oninput = function() {
-  userDefCommRate.innerHTML = this.value;
-}
+slider.addEventListener("input", function () {
+    sliderValue.textContent = slider.value;
+});
