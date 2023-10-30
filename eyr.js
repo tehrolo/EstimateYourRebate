3bfad0cb0a9ceb2771c09225cf24e686f5dca8f2
tehrolo <teheoleo@gmail.com>
2023-10-29 21:12:05 -0500

    Update eyr.js

--- eyr.js
+++ eyr.js
@@ -17,11 +17,8 @@ if(userDonate == True) {
     var EstimatedDonation=NetIncome * 15;
 }
 
-var slider = document.getElementById("myRange");
-var output = document.getElementById("demo");
-output.innerHTML = slider.value; // Display the default slider value
-
-// Update the current slider value (each time you drag the slider handle)
-slider.oninput = function() {
+//update slider
+output.innerHTML = commissionRateSlider.value; // Display the default slider value
+commissionRateSlider.oninput = function() {
   output.innerHTML = this.value;
 }
