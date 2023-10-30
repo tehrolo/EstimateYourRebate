bbe150c6b06d8c3bcc4db341192b628353f1cf67
tehrolo <teheoleo@gmail.com>
2023-10-30 08:34:13 -0500

    Update script.js

--- script.js
+++ script.js
@@ -4,7 +4,35 @@ const yourRebateValue = document.getElementById("yourRebateValue");
 const primaryFeeValue = document.getElementById("primaryFeeValue");
 const secondaryFeeValue = document.getElementById("secondaryFeeValue");
 const commissionValue = document.getElementById("commissionValue");
+const purchasePrice = document.getElementById("buySellPrice");
+var grossCommissionIncome;
+var brokerageSplit;
+var netIncome;
+var estimatedRebate;
+var adjustedCommission;
+var total;
 
+function updateResults() {
+    calculate()
+    grossCommissionIncome.textContent = ;
+    brokerageSplit.textContent = ;
+    netIncome.textContent = ;
+    estimatedRebate.textContent = ;
+    commissionValue.textContent = ;
+    total.textContent = ;
+}
+function calculate() {
+    grossCommissionIncome = (purchasePrice.value * (sliderValue.value / 100));
+    brokerageSplit = (grossCommissionIncome * 0.93);
+    netIncome = (brokerageSplit * 0.95);
+    estimatedRebate = (netIncome * 0.4);
+    commissionValue = (netIncome - estimatedRebate);
+    total = (brokerageSplit + netIncome + estimatedRebate + commissionValue)
+}
 slider.addEventListener("input", function () {
     sliderValue.textContent = slider.value;
+    updateResults()
+});
+purchasePrice.addEventListener("input", function () {
+    updateResults()
 });
