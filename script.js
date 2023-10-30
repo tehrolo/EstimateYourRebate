0617a8b85f77f90071c6a3c885a16fcb5f8277fc
tehrolo <teheoleo@gmail.com>
2023-10-30 18:05:11 -0500

    Update script.js

--- script.js
+++ script.js
@@ -62,7 +62,7 @@ var barColors = [
 // Initialize Chart.js
 const ctx = document.getElementById('myChart').getContext('2d');
 const myChart = new Chart(ctx, {
-  type: 'bar',
+  type: 'doughnut',
   data: {
     labels: labels,
     datasets: [{
